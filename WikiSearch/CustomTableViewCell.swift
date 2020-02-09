b90fd29f5fc61385d2a6599e2866a3a35c729f4c
Charith Lakshitha <charithlakshitha@outlook.com>
2020-02-09 22:32:45 +0530

    creating table classes

---
+++ WikiSearch/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  WikiSearch
+//
+//  Created by Charith Lakshitha on 2/9/20.
+//  Copyright © 2020 Charith Lakshitha. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell{
+    
+    
+    
+}
