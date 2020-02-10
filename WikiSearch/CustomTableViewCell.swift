1c80fb65e54d523a2fcd1f043b01fd333f1076b9
Charith Lakshitha <charithlakshitha@outlook.com>
2020-02-10 17:43:26 +0530

    table class

--- WikiSearch/CustomTableViewCell.swift
+++ WikiSearch/CustomTableViewCell.swift
@@ -2,14 +2,29 @@
 //  CustomTableViewCell.swift
 //  WikiSearch
 //
-//  Created by Charith Lakshitha on 2/9/20.
+//  Created by Charith Lakshitha on 2/10/20.
 //  Copyright © 2020 Charith Lakshitha. All rights reserved.
 //
 
 import UIKit
 
-class CustomTableViewCell: UITableViewCell{
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var wikiImageView: UIImageView!
     
+    @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var descriptionLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
